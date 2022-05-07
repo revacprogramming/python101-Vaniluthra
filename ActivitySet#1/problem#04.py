848419e83290248832e922db280ea16e447a192b
vaniluthra <94587714+Vaniluthra@users.noreply.github.com>
2022-05-07 10:02:24 +0000

    n

--- ActivitySet#1/problem#04.py
+++ ActivitySet#1/problem#04.py
@@ -1,3 +1,32 @@
 # Conditional Execution
+# hrs,r=1,1
+# def loop(x,y):
+#   try:
+#     x= float(input("Enter hours: "))
+#      y= float(input("Enter rate: "))
+#   except:
+#     print('Enter only numerical values...')
+#     loop(x,y)
+#   print(x,y)
+#   return x,y
+  
+# print(hrs,r)
 
-hrs = input("Enter hours? ")
+# hrs,r=loop(hrs,r)
+# print(hrs,r)
+try:
+  hrs= float(input("Enter hours: "))
+  r= float(input("Enter rate: "))
+except:
+  print('Enter only numerical values...')
+  quit()
+if hrs>40:
+  print('overtime!')
+  t=hrs*r
+  extra=(hrs-40)*.5*r
+  pay=t+extra
+else:
+  print('regular!')
+  pay=hrs*r
+print('pay is:',pay)
+
